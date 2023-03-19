d787e468c7b9d6584df1e61f83558365adbd02af
Alceu-wv <alceu.wv@gmail.com>
2023-03-19 20:23:47 -0300

    feat: register view

---
+++ BooksManager.MVC/Controllers/AuthController.cs
@@ -0,0 +1,39 @@
+﻿using BooksManager.Infrastructure.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Formatting;
+
+
+namespace BooksManager.MVC.Controllers
+{
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        public ViewResult Login() => View();
+
+
+        [HttpGet]
+        public ViewResult Register() => View();
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(User model)
+        {
+
+            var httpClient = new HttpClient();
+            var response = httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Post, "https://localhost:7233/api/User")
+            {
+                Content = new ObjectContent<User>(model, new JsonMediaTypeFormatter())
+            }).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Login", "Auth");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Erro ao realizar o cadastro. Verifique os dados inseridos e tente novamente");
+                return View(model);
+            }
+        }
+    }
+}
