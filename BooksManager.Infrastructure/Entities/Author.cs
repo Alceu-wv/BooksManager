3f222e7e54a6c1d1c69ede57630a1908061c1495
Alceu-wv <alceu.wv@gmail.com>
2023-03-25 18:47:30 -0300

    feat: Create Author

--- BooksManager.Infrastructure/Entities/Author.cs
+++ BooksManager.Infrastructure/Entities/Author.cs
@@ -10,5 +10,19 @@
 
         // Relacionamento com livros
         public List<Book> Books { get; set; }
+
+        public Author()
+        {
+            Books = new List<Book>();
+        }
+        public Author(int id, string firstName, string lastName, string email, DateTime birthDate)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            BirthDate = birthDate;
+            Books = new List<Book>(); // Inicializa a propriedade Books como uma lista vazia
+        }
     }
 }
