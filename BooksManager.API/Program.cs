6cb122d27a94816058ed368d5422bbf65d0cdc39
Alceu-wv <alceu.wv@gmail.com>
2023-03-29 08:30:32 -0300

    feat: working token

--- BooksManager.API/Program.cs
+++ BooksManager.API/Program.cs
@@ -7,11 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
-BooksManager.Infrastructure.InversionOFControl.DependencyInjection.Inject(builder.Services, builder.Configuration);
 
 //jwt
 var tokenValidationParams = new TokenValidationParameters()
@@ -20,12 +16,17 @@ var tokenValidationParams = new TokenValidationParameters()
     ValidateAudience = false,
     ValidateIssuer = false
 };
-builder.Services.AddAuthentication()
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
         options.TokenValidationParameters = tokenValidationParams;
     });
 
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+BooksManager.Infrastructure.InversionOFControl.DependencyInjection.Inject(builder.Services, builder.Configuration);
 
 // Configure the HTTP request pipeline.
 var app = builder.Build();
